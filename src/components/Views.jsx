cf37c65ffef723fdbe1ca0866f87d4659a221733
tunny17 <tvnji17@gmail.com>
2023-04-05 21:59:54 +0100

    completion of a dummy line chart

--- src/components/Views.jsx
+++ src/components/Views.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import Chart from 'chart.js/auto';
 
 const Views = () => {
-  return (
-    <div>Views</div>
-  )
-}
+    const chartRef = useRef(null);
 
-export default Views
+    useEffect(() => {
+
+        const chartElement = chartRef.current;
+
+        const chart = new Chart(chartElement, {
+            type: 'line',
+            data: {
+                labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                datasets: [
+                    {
+                        label: 'Sales',
+                        data: [150, 200, 100, 250, 175],
+                        borderColor: 'rgba(255, 84, 3, 1)',
+                        borderWidth: 1,
+                        pointStyle: false,
+                        fill: true,
+                        backgroundColor: 'rgba(255, 84, 3, 0.2)'
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        grid: {
+                            borderDash: [10]
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false
+                        },
+                    },
+                },
+            },
+        });
+
+        return () => {
+            chart.destroy();
+        };
+
+    }, []);
+
+    return (
+        <div>
+            <canvas ref={chartRef} />
+        </div>
+    );
+};
+
+export default Views;
